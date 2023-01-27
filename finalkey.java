a47ea0b597c7f64b0c6934d6c6aef9de05c841e6
Noor Basher <122342208+Noorbasher121@users.noreply.github.com>
2023-01-27 17:54:13 +0530

    Add files via upload

---
+++ finalkey.java
@@ -0,0 +1,31 @@
+
+class A{
+	final int noofalphabate=26;
+	final int noofdigit;
+	A(){
+		noofdigit=10;
+	}
+	final void fun()
+	{
+		System.out.println("Hi, i am function in class A");
+	}
+}
+
+
+ final class B extends A{
+    //final class cannot be inheritance.
+//	void fun() {
+//		System.out.println("Hi, i am function in class A");
+//	}
+}
+
+//class C extends B{
+//	
+//}
+
+
+public class finalkey {
+   public static void main(String[] args) {
+    
+   } 
+}
